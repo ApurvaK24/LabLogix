82458fac7b1cefe10a13dcdd1e6d821dc8eaaa22
Atharva <atharva.j.khodke@gmail.com>
2023-10-18 21:29:06 +0530

    changes in login function

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
 from flask import Flask, render_template
-# from flask_wtf import FlaskForm
-# from wtforms import StringField , SubmitField
-# from wtforms.validators import DataRequired
-
+from flask_wtf import FlaskForm
+from wtforms import StringField , SubmitField
+from wtforms.validators import DataRequired
+from datetime import datetime 
+from flask_sqlalchemy import SQLAlchemy
+# from flask_migrate import Migrate
+from werkzeug.security import generate_password_hash, check_password_hash 
+from datetime import date
+# from webforms import LoginForm, PostForm, UserForm, PasswordForm, NamerForm, SearchForm
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+# from webforms import LoginForm, PostForm, UserForm, PasswordForm, NamerForm
+# from flask_ckeditor import CKEditor
+# from werkzeug.utils import secure_filename
+# import uuid as uuid
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "my super secret key"
+
+# Flask_Login Stuff
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+	return Users.query.get(int(user_id))
+
 
 @app.route('/')
 
@@ -15,7 +36,24 @@ def home():
 @app.route('/login')
 
 def login():
-    return render_template("login.html")
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = Users.query.filter_by(username=form.username.data).first()
+        if user:
+			# Check the hash
+            if check_password_hash(user.password_hash, form.password.data):
+             login_user(user)
+             flash("Login Succesfull!!")
+             return redirect(url_for('dashboard'))
+            else:
+              flash("Wrong Password - Try Again!")
+        else:
+          flash("That User Doesn't Exist! Try Again...")
+
+
+    return render_template('login.html', form=form)
+     
+    # return render_template("login.html")
 
 @app.route('/AboutUs')
 
